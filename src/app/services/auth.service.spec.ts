891806ca6669b6ec7ded7e88d513c26b3d92aae8
Arne Caluwe <arne.caluwe@outlook.com>
2023-10-30 18:05:30 +0100

    add authentication service test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -1,17 +1,54 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
-import { HttpClientModule } from '@angular/common/http';
+import { environment } from '@env/environment';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientModule] });
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    authService = TestBed.inject(AuthService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(authService).toBeTruthy();
+  });
+
+  it('should return an access token', () => {
+    const mockResponse = {
+      access_token: 'mock-access-token',
+      token_type: 'Bearer',
+      expires_in: 3600,
+    };
+
+    authService.getToken$().subscribe(response => {
+      expect(response.access_token).toEqual(mockResponse.access_token);
+      expect(response.token_type).toEqual(mockResponse.token_type);
+      expect(response.expires_in).toEqual(mockResponse.expires_in);
+    });
+
+    const req = httpTestingController.expectOne(`${environment.authApi}token`);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(
+      'grant_type=client_credentials&client_id=' +
+        environment.spotifyClientId +
+        '&client_secret=' +
+        environment.spotifyClientSecret
+    );
+    expect(req.request.headers.get('Content-Type')).toEqual(
+      'application/x-www-form-urlencoded'
+    );
+
+    req.flush(mockResponse);
   });
 });
