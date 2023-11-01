f13c9f6384f9a234fc4ec18c790c90601341cb62
Arne Caluwe <arne.caluwe@outlook.com>
2023-11-01 16:59:21 +0100

    add store caching

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,18 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { environment } from '@env/environment';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { routes } from './app.routes';
+import { authorizationInterceptor } from './interceptors/authorization.interceptor';
 import { SpotifyState } from './state/spotify.state';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authorizationInterceptor])),
     provideAnimations(),
     importProvidersFrom(
       NgxsModule.forRoot([SpotifyState], {
@@ -19,7 +21,8 @@ export const appConfig: ApplicationConfig = {
           suppressErrors: false,
           injectContainerState: false,
         },
-      })
+      }),
+      NgxsStoragePluginModule.forRoot()
     ),
   ],
 };
