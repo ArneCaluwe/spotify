4bbca376c1d90c275b455d573bbfd2b89e0b36cf
Arne Caluwe <arne.caluwe@outlook.com>
2023-11-01 19:17:49 +0100

    add logging for ngxs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,28 +1,40 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import {
+  ApplicationConfig,
+  EnvironmentProviders,
+  importProvidersFrom,
+} from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { environment } from '@env/environment';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
 import { NgxsModule } from '@ngxs/store';
 import { routes } from './app.routes';
 import { authorizationInterceptor } from './interceptors/authorization.interceptor';
 import { SpotifyState } from './state/spotify.state';
 
+const provideNgxs: () => EnvironmentProviders = () =>
+  importProvidersFrom(
+    NgxsModule.forRoot([SpotifyState], {
+      developmentMode: !environment.production,
+      selectorOptions: {
+        suppressErrors: false,
+        injectContainerState: false,
+      },
+    }),
+    NgxsStoragePluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot({
+      disabled: environment.production,
+    })
+  );
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authorizationInterceptor])),
     provideAnimations(),
-    importProvidersFrom(
-      NgxsModule.forRoot([SpotifyState], {
-        developmentMode: !environment.production,
-        selectorOptions: {
-          suppressErrors: false,
-          injectContainerState: false,
-        },
-      }),
-      NgxsStoragePluginModule.forRoot()
-    ),
+    provideNgxs(),
+    importProvidersFrom(),
   ],
 };
