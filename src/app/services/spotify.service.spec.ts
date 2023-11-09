09d1b56e54a90844e064c24b9d8f0b71e6d8a5c8
Arne Caluwe <47561535+ArneCaluwe@users.noreply.github.com>
2023-11-09 21:03:38 +0100

    improve readability by increasing tests and reshaping state (#14)
    * improve readability by increasing tests and reshaping state

--- src/app/services/spotify.service.spec.ts
+++ src/app/services/spotify.service.spec.ts
@@ -3,7 +3,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { Album, AlbumArtist, SpotifyService } from './spotify.service';
+import { Album, AlbumArtist, Artist, SpotifyService } from './spotify.service';
 
 describe('SpotifyService', () => {
   let spotifyService: SpotifyService;
@@ -31,10 +31,10 @@ describe('SpotifyService', () => {
 
       const expectedResponse: {
         albums: Array<Album>;
-        artists: Array<AlbumArtist>;
+        artists: { [key: string]: AlbumArtist };
       } = {
         albums: [],
-        artists: [],
+        artists: {},
       };
 
       spotifyService.getNewReleases$().subscribe(response => {
@@ -47,6 +47,49 @@ describe('SpotifyService', () => {
       expect(req.request.method).toEqual('GET');
 
       req.flush(mockResponse);
+    });
+
+    it('should correctly map albums', () => {
+      const expectedResponse: {
+        albums: Array<Album>;
+        artists: { [key: string]: AlbumArtist };
+      } = {
+        albums: [
+          {
+            href: 'mock-href',
+            id: 'mock-id',
+            images: [
+              {
+                height: 640,
+                url: 'mock-image-url',
+                width: 640,
+              },
+            ],
+            name: 'mock-name',
+            uri: 'mock-uri',
+            artist: 'mock-artist-id',
+          },
+        ],
+        artists: {
+          'mock-artist-id': {
+            href: 'mock-href',
+            id: 'mock-artist-id',
+            name: 'mock-name',
+            uri: 'mock-uri',
+          },
+        },
+      };
+
+      spotifyService.getNewReleases$().subscribe(response => {
+        expect(response).toEqual(expectedResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        `https://api.spotify.com/v1/browse/new-releases`
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(MOCK_ALBUM_RESPONSE);
     });
   });
 
@@ -66,6 +109,35 @@ describe('SpotifyService', () => {
       expect(req.request.method).toEqual('GET');
 
       req.flush(mockResponse);
+    });
+
+    it('should correctly map artists', () => {
+      const expectedResponse: Array<Artist> = [
+        {
+          id: 'mock-id',
+          name: 'mock-name',
+          uri: 'mock-uri',
+          href: 'mock-href',
+          genres: ['mock-genre'],
+          images: [
+            {
+              height: 640,
+              url: 'mock-image-url',
+              width: 640,
+            },
+          ],
+        },
+      ];
+      spotifyService.getRelatedArtists$('mock-artist').subscribe(response => {
+        expect(response).toEqual(expectedResponse);
+      });
+
+      const req = httpTestingController.expectOne(
+        `https://api.spotify.com/v1/artists/mock-artist/related-artists`
+      );
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(MOCK_ARTIST_RESPONSE);
     });
   });
 
@@ -88,3 +160,66 @@ describe('SpotifyService', () => {
     });
   });
 });
+
+const MOCK_ALBUM_RESPONSE = [
+  {
+    album_type: 'mock-album-type',
+    artists: [
+      {
+        external_urls: {
+          spotify: 'mock-artitst-url',
+        },
+        href: 'mock-href',
+        id: 'mock-artist-id',
+        name: 'mock-name',
+        type: 'artist',
+        uri: 'mock-uri',
+      },
+    ],
+    available_markets: 'MOCK-MARKET',
+    external_urls: {
+      spotify: 'mock-spotify-url',
+    },
+    href: 'mock-href',
+    id: 'mock-id',
+    images: [
+      {
+        height: 640,
+        url: 'mock-image-url',
+        width: 640,
+      },
+    ],
+    name: 'mock-name',
+    release_date: 'mock-release-date',
+    release_date_precision: 'day',
+    total_tracks: 5,
+    type: 'album',
+    uri: 'mock-uri',
+  },
+];
+
+const MOCK_ARTIST_RESPONSE = [
+  {
+    external_urls: {
+      spotify: 'mock-spotify-url',
+    },
+    followers: {
+      href: null,
+      total: 0,
+    },
+    genres: ['mock-genre'],
+    href: 'mock-href',
+    id: 'mock-id',
+    images: [
+      {
+        height: 640,
+        url: 'mock-image-url',
+        width: 640,
+      },
+    ],
+    name: 'mock-name',
+    popularity: 0,
+    type: 'artist',
+    uri: 'mock-uri',
+  },
+];
