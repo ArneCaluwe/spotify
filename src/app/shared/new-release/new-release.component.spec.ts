8bf1807b8623f622959f326ef968b84bcb747145
Arne Caluwe <47561535+ArneCaluwe@users.noreply.github.com>
2023-11-13 20:47:21 +0100

    move new-release logic to new Standalone component (#15)
    * move new-release logic to new Standalone component
    
    * temporarily set html parsing rule fro prettier to warning
    [Clashes with eslint]
    
    * update angular version
    
    * exclude state from code coverage reports
    
    * revert temporary linting change

---
+++ src/app/shared/new-release/new-release.component.spec.ts
@@ -0,0 +1,49 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SpotifyState } from '@app/state';
+import { NgxsModule, Store } from '@ngxs/store';
+import { NewReleaseComponent } from './new-release.component';
+
+describe('NewReleaseComponent', () => {
+  let component: NewReleaseComponent;
+  let fixture: ComponentFixture<NewReleaseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        NewReleaseComponent,
+        NgxsModule.forRoot([SpotifyState]),
+        HttpClientTestingModule,
+      ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NewReleaseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should get albums from state', done => {
+    component.albumId = '1';
+    const store = TestBed.inject(Store);
+    const state = {
+      spotify: {
+        albums: [
+          { id: '1', name: 'Album 1', images: [{}, {}, { url: 'some-url' }] },
+        ],
+      },
+    };
+    store.reset(state);
+
+    fixture.detectChanges();
+    component.album$.subscribe(album => {
+      expect(album?.id).toEqual('1');
+      expect(album?.name).toEqual('Album 1');
+      done();
+    });
+  });
+});
